528b6c654d7979be173b79b85af9cc3654f52251
susshikan <muhamadh123g@gmail.com>
2025-11-16 08:06:06 +0700

    feat: Add docker

--- socket/src/ws.ts
+++ socket/src/ws.ts
@@ -46,6 +46,7 @@ async function start() {
         id: msg.id,
         payload: msg.message,
       };
+      console.log(json)
       wss.clients.forEach(c => c.send(JSON.stringify(json)));
     });
   }
